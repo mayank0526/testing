e6020092355d1648c12bf736a39f2320877542ed
Mayank.2.Gupta <Mayank.2.Gupta@LP-TZD-5X045727.IN.COFORGETECH.COM>
2026-01-14 15:34:26 +0530

    final 10

--- Testing/src/Pack1/lab10.java
+++ Testing/src/Pack1/lab10.java
@@ -2,7 +2,7 @@ package Pack1;
 
 public class lab10 {
 	public static boolean StringIsPositive(String str) {
-		
+		  String sb = str.toUpperCase();
 		  if(str.length() == 0) return true;
 		   
 		  
@@ -12,7 +12,7 @@ public class lab10 {
 		 return true;
 	  }
 	  public static void main(String args[]) {
-		  String str = "aBc";
+		  String str = "mayank";
 		  
 		  System.out.println(StringIsPositive(str));
 
