87455acd7faabb321498b8725112960f9ca4a781
Mayank.2.Gupta <Mayank.2.Gupta@LP-TZD-5X045727.IN.COFORGETECH.COM>
2026-01-17 15:49:15 +0530

    kk

---
+++ Testing/src/Pack1/arraylist.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+import java.util.*;
+
+
+public class arraylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer>list = new ArrayList<>();
+		list.add(30);
+		list.add(1, 32);
+		int total = list.get(0);
+
+	}
+
+}
